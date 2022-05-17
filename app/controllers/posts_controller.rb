44346786c4c5cec2923555abdb55788371b11963
afagan5 <abby.fagan@kellogg.northwestern.edu>
2022-05-17 22:08:07 +0000

    tues afternoon changes v3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,9 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        @post["place_id"] = @post["id"]
+    
       end
     
       def create
@@ -14,7 +17,7 @@ class PostsController < ApplicationController
         @post["posted_on"] = params["post"]["posted_on"]
         @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places"
       end
 
 
