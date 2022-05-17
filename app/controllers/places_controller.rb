eeb4f48b1db964bd30e09c20f2d1dfbfaf9507a5
afagan5 <abby.fagan@kellogg.northwestern.edu>
2022-05-17 21:42:45 +0000

    tuesday afternoon changes3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,9 +5,19 @@ class PlacesController < ApplicationController
     
       def show
         @place = Place.find_by({ "id" => params["id"] })
+        @posts = Post.where({ "place_id" => @place["id"] })
       end 
-    end
+
 
     def new
         @place = Place.new
       end
+    
+      def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+
+    end
